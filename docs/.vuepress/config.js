ed14b6a0b0b6d45f05bfadc983200821dd6d2f3c
AlrclyForger <Alrcly@Foxmail.com>
2025-10-23 23:17:30 +0800

    全新的版本，全新的开始。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
-import { katexPlugin } from '@renovamen/vuepress-plugin-katex'
-import { mermaidPlugin } from '@renovamen/vuepress-plugin-mermaid'
+import { viteBundler } from '@vuepress/bundler-vite'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { markdownChartPlugin } from '@vuepress/plugin-markdown-chart'
+import { markdownMathPlugin } from '@vuepress/plugin-markdown-math'
+import { prismjsPlugin } from '@vuepress/plugin-prismjs'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
@@ -13,10 +15,18 @@ export default defineUserConfig({
   head: [
     ['link', { rel: 'icon', href: '/vuepress.ico' }],
     ['link', { rel: 'apple-touch-icon', href: '/vuepress.ico' }],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+      },
+    ],
   ],
   plugins: [
-    mermaidPlugin(),
-    katexPlugin(),
+    markdownChartPlugin({ mermaid: true }),
+    markdownMathPlugin(),
+    prismjsPlugin(),
     docsearchPlugin({
       appId: 'IYMYJMZXL9',
       apiKey: '111e0f1ad4f57a9b528863307555ba2e',
@@ -66,12 +76,11 @@ export default defineUserConfig({
       },
     }),
   ],
+  bundler: viteBundler(),
   theme: defaultTheme({
-    contributors: false,
-    lastUpdated: false,
     navbar: [
       {
-        text: '示波案边',
+        text: '电子电路',
         children: [
           {
             text: '基础部分',
@@ -84,153 +93,12 @@ export default defineUserConfig({
           },
         ],
       },
-      {
-        text: '音乐让我说',
-        children: [
-          {
-            text: '叨叨冯聊音乐',
-            children: [
-              {
-                text: '基础乐理课堂',
-                link: '/music/theory/01.音名、五线谱谱号与音高.html',
-              },
-            ],
-          },
-        ],
-      },
-      {
-        text: '编程之道',
-        children: [
-          {
-            text: '基础部分',
-            children: [
-              {
-                text: 'CSS',
-                link: '/code/css/11.GRID 栅格系统.html',
-              },
-              {
-                text: 'JavaScript',
-                link: '/code/javascript/01.走进 JavaScript 黑洞.html',
-              },
-              {
-                text: 'TypeScript',
-                link: '/code/typescript/01.TypeScript 变量.html',
-              },
-              {
-                text: 'Node.JS',
-                link: '/code/node/01.NVM 和 NPM.html',
-              },
-              {
-                text: 'PostgreSQL',
-                link: '/code/postgre/1.基本操作.html',
-              },
-            ],
-          },
-          {
-            text: '框架部分',
-            children: [
-              {
-                text: 'Vue.JS',
-                link: '/code/vue/01.为什么选择.html',
-              },
-              {
-                text: 'NestJS',
-                link: '/code/nestjs/01.解释 NestJS.html',
-              },
-            ],
-          },
-          {
-            text: '拓展部分',
-            children: [
-              {
-                text: 'Docker',
-                link: '/code/docker/docker.html',
-              },
-              {
-                text: 'HTTP 协议',
-                link: '/code/http/01.破冰篇.html',
-              },
-            ],
-          },
-        ],
-      },
     ],
     sidebar: {
       // 电子电路
       '/electronics/circuit/': [
         '/electronics/circuit/01.电流.html',
         '/electronics/circuit/02.电压.html',
-      ],
-      // 音乐
-      '/music/theory/': [
-        '/music/theory/01.音名、五线谱谱号与音高.html',
-        '/music/theory/02.小节、力度记号与反复记号.html',
-        '/music/theory/03.认识音符、休止符、时值与拍号.html',
-        '/music/theory/04.特殊拍号、数拍方法、符点与连线.html',
-        '/music/theory/05.认识、理解大小调音阶及调号.html',
-      ],
-      // 编程
-      '/code/css/': ['/code/css/11.GRID 栅格系统.html'],
-      '/code/javascript/': ['/code/javascript/01.走进 JavaScript 黑洞.html'],
-      '/code/typescript/': [
-        '/code/typescript/01.TypeScript 变量.html',
-        '/code/typescript/02.TypeScript 函数.html',
-        '/code/typescript/03.TypeScript 类.html',
-        '/code/typescript/04.TypeScript 接口.html',
-        '/code/typescript/05.TypeScript 装饰器.html',
-        '/code/typescript/06.TypeScript 命名空间.html',
-        '/code/typescript/07.TypeScript 类型工具.html',
-      ],
-      '/code/node/': ['/code/node/01.NVM 和 NPM.html'],
-      '/code/postgre/': [
-        '/code/postgre/1.基本操作.html',
-        '/code/postgre/2.数据类型、运算符.html',
-        '/code/postgre/3.数据库函数.html',
-        '/code/postgre/4.数据库索引.html',
-        '/code/postgre/5.数据库视图.html',
-        '/code/postgre/6.CRUD.html',
-        '/code/postgre/7.主键和外键.html',
-        '/code/postgre/8.数据表约束.html',
-        '/code/postgre/9.表查询操作.html',
-        '/code/postgre/10.子查询.html',
-        '/code/postgre/11.结构集合并.html',
-        '/code/postgre/12.备份、恢复数据库.html',
-      ],
-      '/code/vue/': [
-        '/code/vue/01.为什么选择.html',
-        '/code/vue/02.快速开始.html',
-      ],
-      '/code/nestjs/': [
-        '/code/nestjs/02.ICO 控制反转 DI 依赖注入.html',
-        '/code/nestjs/03.装饰器.html',
-        '/code/nestjs/05.NestJS CLI.html',
-        '/code/nestjs/07.RESTful 风格设计.html',
-        '/code/nestjs/08.NestJS 控制装饰器.html',
-        '/code/nestjs/09.session.html',
-        '/code/nestjs/10.NestJS 提供者 (providers).html',
-        '/code/nestjs/11.NestJS 模块 (modeule.ts).html',
-        '/code/nestjs/12.NestJS 中间件 (middleware.ts).html',
-        '/code/nestjs/13.NestJS 上传文件.html',
-        '/code/nestjs/14.NestJS 下载文件.html',
-        '/code/nestjs/15.NestJS Rxjs.html',
-        '/code/nestjs/16.NestJS 响应拦截器.html',
-        '/code/nestjs/17.NestJS 异常拦截器.html',
-        '/code/nestjs/18.NestJS 管道.html',
-        '/code/nestjs/19.NestJS 爬虫.html',
-        '/code/nestjs/20.NestJS 守卫.html',
-        '/code/nestjs/21.NestJS 自定义装饰器.html',
-        '/code/nestjs/22.NestJS swagger 接口文档.html',
-        '/code/nestjs/23.NestJS 连接数据库.html',
-        '/code/nestjs/24.NestJS 实体.html',
-        '/code/nestjs/25.NestJS CURD.html',
-        '/code/nestjs/26.NestJS 分页排序.html',
-        '/code/nestjs/27.NestJS 多表联查.html',
-        '/code/nestjs/28.NestJS 事物.html',
-      ],
-      '/code/http/': [
-        '/code/http/01.破冰篇.html',
-        '/code/http/02.基础篇.html',
-        '/code/http/03.进阶篇.html',
       ],
     },
   }),
